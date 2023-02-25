9b88c374c0f82d15fdba817eebeba8b6491758cb
Nathan Clevenger <nateclev@gmail.com>
2023-02-25 09:17:46 -0600

    Removed dashboard header

--- worker.js
+++ worker.js
@@ -87,15 +87,9 @@ export default {
         </div>
       </div>
     </nav>
-
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-      </div>
-    </header>
     <main>
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <div id="container" style="height: 100vh; border:0px;"></div>
+        <div id="container" class="h-100"></div>
       </div>
     </main>
   </div>
