11652150bc81d18a831ea9ee02c1abb716bfe4ec
Nathan Clevenger <nateclev@gmail.com>
2023-02-25 08:44:56 -0600

    Updated style

--- worker.js
+++ worker.js
@@ -5,36 +5,40 @@ export default {
     const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
     
     // TODO: Implement this
-    const [ resource, id ] = pathSegments
-    const data = { resource, id, hello: user.city }
-    
+    // const [ resource, id ] = pathSegments
+    // const data = { resource, id, hello: user.city }
+
+    const res = pathname == '/' ? undefined : await fetch('https:/' + pathname)
+    const body = res ? await res.text() : undefined
+    const contentType = res?.headers?.get('content-type')
+    const type = contentType.includes('html') ? 'html' : contentType.includes('json') ? 'json' : contentType.includes('javascript') ? 'javascript' : 'text'
+    const html = `<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Monaco editor</title>
+<link rel="stylesheet" data-name="vs/editor/editor.main" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/editor/editor.main.min.css">
+</head>
+<body>
+<div id="container" style="height:100%; border:0px;"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/loader.min.js"></script>
+<script>
+// require is provided by loader.min.js.
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs' }});
+require(["vs/editor/editor.main"], () => {
+  monaco.editor.create(document.getElementById('container'), {
+    value: \`${body}\`,
+    language: '${type}',
+    theme: 'vs-dark',
+  });
+});
+</script>
+</body>
+</html>`
+
     return new Response(html, { headers: { 'content-type': 'text/html; charset=utf-8' }})
   }
 }
 
-const html = `<!DOCTYPE html>
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Monaco editor</title>
-  <link rel="stylesheet" data-name="vs/editor/editor.main" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/editor/editor.main.min.css">
-</head>
-<body>
-  <div id="container" style="height:400px;border:1px solid black;"></div>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/loader.min.js"></script>
-  <script>
-    // require is provided by loader.min.js.
-    require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs' }});
-    require(["vs/editor/editor.main"], () => {
-      monaco.editor.create(document.getElementById('container'), {
-        value: \`function x() {
-      console.log("Hello world!");
-    }\`,
-        language: 'javascript',
-        theme: 'vs-dark',
-      });
-    });
-  </script>
-</body>
-</html>`
+
