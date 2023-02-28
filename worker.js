641e12877d0de7b934978332a6c917e94cd2d0bd
Nathan Clevenger <nateclev@gmail.com>
2023-02-28 02:40:46 -0600

    Trying to fix Monaco

--- worker.js
+++ worker.js
@@ -13,21 +13,14 @@ export default {
     const type = contentType?.includes('html') ? 'html' : contentType?.includes('json') ? 'json' : contentType?.includes('javascript') ? 'javascript' : 'text'
     const html = `<!DOCTYPE html>
 <!doctype html>
-<html class="h-full bg-gray-100">
-<head>
+<html lang="en">
+  <head>
   <meta charset="utf-8">
   <title>Monaco editor</title>
-  <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" data-name="vs/editor/editor.main" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/editor/editor.main.min.css">
 </head>
-<body class="h-full">
-  <div class="min-h-full">
-    <main class="h-full">
-      <div id="container" class="h-full"></div>
-    </main>
-  </div>
-
-  
+<body style="margin: 0;">
+  <div id="container" style="height: 100vh; border:0px;"></div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.0/min/vs/loader.min.js"></script>
   <script>
   // require is provided by loader.min.js.
